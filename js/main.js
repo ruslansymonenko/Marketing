f24dc2ede3025133cf31b061cb9a2e87524e6996
Ruslan Symonenko <symonenkors@gmail.com>
2022-10-29 16:35:29 +0300

    finished slider-blog website block

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function(){
     var mixer = mixitup('.portfolio__content');
 
+    let leftArrPath = '<img src="./images/arr-left.svg" alt="left arrow">';
+    let rightArrPath = '<img src="./images/arr-right.svg" alt="rihgt arrow">';
+
     $(".youtube-link").grtyoutube();
+
+    $(".slider-blog__inner").slick({
+        arrows: true,
+        dots: true,
+        prevArrow: `<button type="button" class="slick-prev">${leftArrPath}</button>`,
+        nextArrow: `<button type="button" class="slick-next">${rightArrPath}</button>`
+    });
 });
 
 
