934e66ebf2a4717232ece93948e8895b4d727d2e
Ruslan Symonenko <symonenkors@gmail.com>
2022-11-03 15:13:53 +0200

    made adaptive header

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
 $(function(){
-    var mixer = mixitup('.portfolio__content');
+    // var mixer = mixitup('.portfolio__content');
 
-    let leftArrPath = '<img src="./images/arr-left.svg" alt="left arrow">';
-    let rightArrPath = '<img src="./images/arr-right.svg" alt="rihgt arrow">';
+    // let leftArrPath = '<img src="./images/arr-left.svg" alt="left arrow">';
+    // let rightArrPath = '<img src="./images/arr-right.svg" alt="rihgt arrow">';
 
-    $(".youtube-link").grtyoutube();
+    // $(".youtube-link").grtyoutube();
 
-    $(".slider-blog__inner").slick({
-        arrows: true,
-        dots: true,
-        prevArrow: `<button type="button" class="slick-prev">${leftArrPath}</button>`,
-        nextArrow: `<button type="button" class="slick-next">${rightArrPath}</button>`
+    // $(".slider-blog__inner").slick({
+    //     arrows: true,
+    //     dots: true,
+    //     prevArrow: `<button type="button" class="slick-prev">${leftArrPath}</button>`,
+    //     nextArrow: `<button type="button" class="slick-next">${rightArrPath}</button>`
+    // });
+
+    $('.menu__btn').on('click', () => {
+        $('.menu__list').toggleClass('menu__list--active');
     });
 });
 
